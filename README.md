4448e5ba778b678ce3986c6ed3aa62997024c444
Jayjeet Chakraborty <jc.github@rediffmail.com>
2019-02-27 23:25:21 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# GIT Workshop 
+
+## Building from source
+
+* g++ -std=c++11 code.cpp
+* ./a.out
---
+++ code.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 3;
+    int b = 5;
+    cout << "Addition : " << a + b << endl;
+}
